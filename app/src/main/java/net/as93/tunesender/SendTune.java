acdf6cbde9aaf7dc6cdf5f72a230350ed756a818
Alicia Sykes <sykes.alicia@gmail.com>
2015-11-16 16:26:39 +0000

    Empty class for sending tune

---
+++ app/src/main/java/net/as93/tunesender/SendTune.java
@@ -0,0 +1,17 @@
+package net.as93.tunesender;
+
+
+
+public class SendTune {
+
+    private Tune tune;
+
+    public SendTune(Tune tune) {
+        this.tune = tune;
+    }
+
+    public boolean sendSMS(){
+
+        return true;
+    }
+}
